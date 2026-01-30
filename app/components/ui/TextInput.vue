ed1594762994ec2f03e83e41939a33827a40be08
Joice Figueiredo <joicebf1995@outlook.com>
2026-01-30 18:53:26 -0300

    Para poder continuar em outra maquina.

---
+++ app/components/ui/TextInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input
+    class="input"
+    :placeholder="placeholder"
+    v-model="model"
+  />
+</template>
+
+<script setup lang="ts">
+const model = defineModel<string>()
+
+defineProps<{
+  placeholder?: string
+}>()
+</script>
+
+<style scoped>
+.input {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
